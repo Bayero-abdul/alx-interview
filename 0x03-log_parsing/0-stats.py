96eab05fe1e8a271959663c57a7661b3bd032769
Bayero-abdul <bayeroa65@gmail.com>
2023-10-19 19:12:14 +0100

    Change regex and put back finally block

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,12 +13,15 @@ if __name__ == "__main__":
     possible_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     status_codes = {}
 
-    pattern = r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - \[\d{4}-\d{2}-\d{2} '
-    r'\d{2}:\d{2}:\d{2}\.\d+\] \"GET \/projects\/260 HTTP\/1\.1\" \d+ \d+'
+    regex = (r'\d+.\d+.\d+.\d+\s?-\s?\[\d+-\d+\d+-\d+ \d+:\d+:\d+\.\d+\] '
+             r'\"GET \/projects\/260 HTTP\/1\.1\" (\d+) \d+')
+
+    # pattern = r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - \[\d{4}-\d{2}-\d{2} '
+    # r'\d{2}:\d{2}:\d{2}\.\d+\] \"GET \/projects\/260 HTTP\/1\.1\" \d+ \d+'
 
     try:
-        for line in sys.stdin:
-            match = re.search(pattern, line.strip())
+        for line in sys.stdin:  
+            match = re.search(regex, line.strip())
             if match:
                 line_args = line.strip().split()
                 file_size = line_args[-1]
@@ -30,8 +33,7 @@ if __name__ == "__main__":
                 except ValueError:
                     pass
 
-                status_codes[status_code] = status_codes.get(
-                    status_code, 0) + 1
+                status_codes[status_code] = status_codes.get(status_code, 0) + 1
 
             if line_count == 10:
                 line_count = 0
@@ -46,6 +48,8 @@ if __name__ == "__main__":
                         print('{}: {}'.format(k, v))
             sys.stdin.flush()
     except (KeyboardInterrupt, EOFError) as e:
+        print()
+    finally:
         print('File size: {}'.format(total_size))
         sorted_codes = dict(
             sorted(
