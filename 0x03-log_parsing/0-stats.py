68028d3e1160240d655dcebebf290124a3f12d00
Bayero-abdul <bayeroa65@gmail.com>
2023-10-19 19:26:06 +0100

    Fix code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,18 +10,22 @@ if __name__ == "__main__":
     line_count = 0
     total_size = 0
 
-    possible_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    status_codes = {}
+    status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0}
 
-    regex = (r'\d+.\d+.\d+.\d+\s?-\s?\[\d+-\d+\d+-\d+ \d+:\d+:\d+\.\d+\] '
-             r'\"GET \/projects\/260 HTTP\/1\.1\" (\d+) \d+')
-
-    # pattern = r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - \[\d{4}-\d{2}-\d{2} '
-    # r'\d{2}:\d{2}:\d{2}\.\d+\] \"GET \/projects\/260 HTTP\/1\.1\" \d+ \d+'
+    pattern = r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - \[\d{4}-\d{2}-\d{2} '
+    r'\d{2}:\d{2}:\d{2}\.\d+\] \"GET \/projects\/260 HTTP\/1\.1\" \d+ \d+'
 
     try:
-        for line in sys.stdin:  
-            match = re.search(regex, line.strip())
+        for line in sys.stdin:
+            match = re.search(pattern, line.strip())
             if match:
                 line_args = line.strip().split()
                 file_size = line_args[-1]
@@ -33,28 +37,22 @@ if __name__ == "__main__":
                 except ValueError:
                     pass
 
-                status_codes[status_code] = status_codes.get(status_code, 0) + 1
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
 
             if line_count == 10:
                 line_count = 0
 
                 print('File size: {}'.format(total_size))
-                sorted_codes = dict(
-                    sorted(
-                        status_codes.items(),
-                        key=lambda item: item[0]))
-                for k, v in sorted_codes.items():
-                    if k in possible_codes:
-                        print('{}: {}'.format(k, v))
+                for code, count in status_codes.items():
+                    if count > 0:
+                        print('{}: {}'.format(code, count))
+
             sys.stdin.flush()
     except (KeyboardInterrupt, EOFError) as e:
         print()
     finally:
         print('File size: {}'.format(total_size))
-        sorted_codes = dict(
-            sorted(
-                status_codes.items(),
-                key=lambda item: item[0]))
-        for k, v in sorted_codes.items():
-            if k in possible_codes:
-                print('{}: {}'.format(k, v))
+        for code, count in status_codes.items():
+            if count > 0:
+                print('{}: {}'.format(code, count))
